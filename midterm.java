09adc178efeac60a5428e88b4029dbba711a86dd
Martin Sit <46298951+martin226@users.noreply.github.com>
2023-04-21 13:06:49 -0400

    feat: remake scene 1

--- midterm.java
+++ midterm.java
@@ -1,41 +1,44 @@
 import arc.*;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.image.BufferedImage;
 
 public class midterm {
-	public static void main(String[] args) {
-		Console con = new Console("The Puzzle Jungle", 1280, 720);
+	private static Console con = new Console("The Puzzle Jungle", 1280, 720);
+	private static Font fntTitle = con.loadFont("fonts/Cousine-Regular.ttf", 16);
+	private static Font fntText = con.loadFont("fonts/Cousine-Regular.ttf", 18);
+	private static Color clrGreekVilla = new Color(237, 234, 224);
+	private static Color clrSage = new Color(207,225,201);
 
+	public static void main(String[] args) {
 		int intChoice;
 		String strChoice;
-		Font fntTitle = con.loadFont("fonts/Cousine-Regular.ttf", 16);
-		Font fntText = con.loadFont("fonts/NotoSans-Regular.ttf", 18);
-		Color clrGreekVilla = new Color(237, 234, 224);
-		
+
 		String strName;
 		int intDice;
 		double dblSolution;
 
-		resetConsole(con, clrGreekVilla);
+		resetConsole(clrGreekVilla);
 
 		con.setTextFont(fntTitle);
-		printTitle(con);
+		printTitle();
 		con.println("Press any key to start...");
 		con.getKey();
-		resetConsole(con, clrGreekVilla);
+		resetConsole(clrGreekVilla);
 
-		con.setTextFont(fntText);
-		scene1(con);
-		con.println(formatInstructions("Enter [1] to follow the path into the jungle.", "Enter [2] to return home."));
+		scene1();
+
+		createChoicebox("Enter your choice:");
 		intChoice = con.readInt();
+
 		// TODO: Input validation function that checks for errors as well
 		while (intChoice != 1 && intChoice != 2) {
 			con.println("Invalid choice. Please try again.");
 			intChoice = con.readInt();
 		}	
-		resetConsole(con, clrGreekVilla);
+		resetConsole(clrGreekVilla);
 		if (intChoice == 1) {
-			scene2(con);
+			scene2();
 			con.println(formatInstructions("Enter [yes] if you want to provide the man your name.", "Enter [no] if you do not want to provide the man your name."));
 			strChoice = con.readLine();
 			while (!strChoice.equalsIgnoreCase("yes") && !strChoice.equalsIgnoreCase("no")) {
@@ -45,9 +48,9 @@ public class midterm {
 			if (strChoice.equalsIgnoreCase("yes")) {
 				con.println(formatInstructions("Enter your name:"));
 				strName = con.readLine();
-				resetConsole(con, clrGreekVilla);
+				resetConsole(clrGreekVilla);
 				
-				scene5(con, strName);
+				scene5(strName);
 				con.println(formatInstructions("Press any key to roll the dice."));
 				con.getKey();
 				
@@ -55,29 +58,29 @@ public class midterm {
 				con.println("You rolled a " + intDice + "!");
 				
 				if (1 <= intDice && intDice <= 3) {
-					scene6(con);
+					scene6();
 					con.println(formatInstructions("What is the solution?"));
 					dblSolution = con.readDouble();
 				} else if (4 <= intDice && intDice <= 6) {
 					// Scene 7
 				}
 			} else if (strChoice.equalsIgnoreCase("no")) {
-				resetConsole(con, clrGreekVilla);
-				scene4(con);
+				resetConsole(clrGreekVilla);
+				scene4();
 				return;
 			}
 		} else if (intChoice == 2) {
-			scene3(con);
+			scene3();
 			return;
 		}
 	}
-	private static void resetConsole(Console con, Color clrBg) {
+	private static void resetConsole(Color clrBg) {
 		con.clear();
 		con.setTextColor(Color.BLACK);
 		con.setDrawColor(clrBg);
 		con.fillRect(0,0,1280,720);
 	}
-	private static void printTitle(Console con) {
+	private static void printTitle() {
 		con.println("████████╗██╗░░██╗███████╗  ██████╗░██╗░░░██╗███████╗███████╗██╗░░░░░███████╗  ░░░░░██╗██╗░░░██╗███╗░░██╗░██████╗░██╗░░░░░███████╗");
 		con.println("╚══██╔══╝██║░░██║██╔════╝  ██╔══██╗██║░░░██║╚════██║╚════██║██║░░░░░██╔════╝  ░░░░░██║██║░░░██║████╗░██║██╔════╝░██║░░░░░██╔════╝");
 		con.println("░░░██║░░░███████║█████╗░░  ██████╔╝██║░░░██║░░███╔═╝░░███╔═╝██║░░░░░█████╗░░  ░░░░░██║██║░░░██║██╔██╗██║██║░░██╗░██║░░░░░█████╗░░");
@@ -93,7 +96,7 @@ public class midterm {
 		int intDice = (int)(Math.random() * 6 + 1);
 		return intDice;
 	}
-	private static void createTextbox(Console con, Color clrText, Color clrBox, Font fntText, String... strText) {
+	private static void createTextbox(Color clrText, Color clrBox, Font fntText, String... strText) {
 		int intPointSize = fntText.getSize();
 		double dblPixelSize = intPointSize * (4.0 / 3.0); // 4/3 is the ratio of pixels to points
 		int intWidth = (int)(1280 * 3 / 4);  // 75% of screen width
@@ -112,41 +115,53 @@ public class midterm {
 			con.drawString(strText[intCount], intX + 15, intY + 20 + (int)Math.ceil(dblPixelSize * intCount));
 		}
 	}
-	private static void scene1(Console con) {
-		// TODO: Add scene image
-		con.println("As you awaken, you find yourself in the heart of a dense jungle.");
-		con.sleep(1000);
-		con.println("Ahead of you lies a winding path that disappears into the distant horizon, beckoning you to follow its mysterious course.");
-		con.sleep(1500);
-		con.println("Standing beside the path, a strange portal emanates a faint, familiar energy of home.");
-		con.sleep(1000);
-		con.println("And there, between the two, a weathered sign reads a cryptic message:\n");
-		con.sleep(1000);
-		con.println("	Will you dare to explore this unknown wilderness,");
-		con.println("	risking all for the promise of untold wealth and discovery?");
-		con.println("	Or will you choose to retreat to the safety and familiarity of your home?");
-		con.println("	The answer lies within you, waiting to be discovered.");
-		con.sleep(2000);
+	private static void createChoicebox(String strPrompt) {
+		con.print(strPrompt + " ");
+		con.setDrawColor(clrGreekVilla);
+		con.setTextColor(Color.BLACK);
+		con.fillRect(0, 0, 1280, 24); // 24 pix is the height of the text
 	}
-	private static void scene2(Console con) {
+	private static void scene1() {
+		BufferedImage imgScene = con.loadImage("img/scene1.png");
+		con.drawImage(imgScene, 0, 0);
+		createTextbox(
+			Color.BLACK, 
+			clrSage, 
+			fntText,
+			"As you awaken, you find yourself in the heart of a dense jungle. Ahead of you lies",
+			"a winding path that disappears into the distant horizon, beckoning you to follow",
+			"its mysterious course. Standing beside the path, a strange portal emanates a faint,",
+			"familiar energy of home. And there, between the two, a weathered sign reads a",
+			"cryptic message:",
+			"",
+			"	Will you dare to explore this unknown wilderness, risking all for the promise",
+			"	of untold wealth and discovery? Or will you choose to retreat to the safety",
+			"	and familiarity of your home? The answer lies within you, waiting to be",
+			"	discovered.",
+			"",
+			"Enter [1] to follow the path into the jungle.",
+			"Enter [2] to return home."
+		);
+	}
+	private static void scene2() {
 		con.println("As you walk down the path, a man emerges from the shadows.");
 		con.sleep(1000);
 		con.println("He introduces himself as a guide, and asks for your name.");
 		con.sleep(2000);
 	}
-	private static void scene3(Console con) {
+	private static void scene3() {
 		con.println("You return to the comfort of your home, empty-handed");
 		con.sleep(2000);
 		con.println("\n-THE END-");
 	}
-	private static void scene4(Console con) {
+	private static void scene4() {
 		con.println("Offended, the guide leaves you to fend for yourself in your unfamiliar surroundings.");
 		con.sleep(1000);
 		con.println("Eventually, you starve to death.");
 		con.sleep(2000);
 		con.println("\n-THE END-");
 	}
-	private static void scene5(Console con, String strName) {
+	private static void scene5(String strName) {
 		con.println("\"Welcome to the Puzzle Jungle, " + strName + "\", the man says.");
 		con.sleep(1000);
 		con.println("He takes you through the thick foliage of the jungle, and you arrive at a vault.");
@@ -156,7 +171,7 @@ public class midterm {
 		con.println("Confused, you turn to the guide, but he has disappeared.");
 		con.sleep(2000);
 	}
-	private static void scene6(Console con) {
+	private static void scene6() {
 		con.println("Congratulations, you have rolled the easy question.");
 		con.sleep(1000);
 		con.println("Name the first 3 digits of pi to receive a diamond.");
